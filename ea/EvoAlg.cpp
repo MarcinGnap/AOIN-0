08f5be365a39d326d0a0ff4b8f27e8162d293734
Marcin Gnap <80780114+MarcinGnap@users.noreply.github.com>
2024-10-14 18:39:48 +0200

    1.5? - tsp started

---
+++ ea/EvoAlg.cpp
@@ -0,0 +1,8 @@
+#include "EvoAlg.h"
+
+float EvoAlg::calculateDistance(float firstX, float firstY, float secondX, float secondY)
+{
+	float fullDistance, xDistance, yDistance;
+
+	return 0.0f;
+}
---
+++ ea/EvoAlg.h
@@ -0,0 +1,10 @@
+#pragma once
+class EvoAlg
+{
+public:
+
+	float calculateDistance(float firstX, float firstY, float secondX, float secondY);
+	
+
+};
+
